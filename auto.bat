729848a0f217f773affba4c8cc708d61fb9cf6c9
mykingback <502276211@qq.com>
2026-04-22 14:41:52 +0800

    auto sync

--- auto.bat
+++ auto.bat
@@ -1,13 +1,15 @@
 @echo off
 chcp 65001
-cd /d "D:\APP6\cm节点池\output" >nul 2>&1
+cd /d "D:\APP6\cm节点池\output"
 
 :loop
-git add . >nul 2>&1
-git diff --cached --quiet >nul 2>&1
+git add .
+git diff --cached --quiet
 if errorlevel 1 (
-    git commit -m "auto sync" >nul 2>&1
-    git push >nul 2>&1
+    git commit -m "auto sync"
+    git push
+    echo ? 同步成功
 )
+echo 等待下一次检查...
 timeout /t 10 /nobreak >nul
 goto loop
